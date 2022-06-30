f595aa1cb075c0d8658a42fb7784869b1f315323
tnr414 <tnr414@gmail.com>
2022-07-01 00:44:35 +0530

    server file

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(express.static("frontend/build"));
 //     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
 // });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000; 
 console.log(port);
 app.listen(port, () => console.log('listing to port ', port));
 module.exports = app;
